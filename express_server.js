1a88725b0ee35d0dfbbef5ca185d756e5a004e1b
peachykeen5 <lexi.keen@gmail.com>
2018-06-12 23:08:40 +0000

    initial commit

---
+++ express_server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+var urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+};
+
+app.get("/urls", (req, res) => {
+    let templateVars = {
+        urls: urlDatabase
+    };
+    res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+    let longURL = urlDatabase[req.params.id];
+    let templateVars = {
+        shortURL: req.params.id,
+        longURL: longURL
+    };
+    res.render("urls_show", templateVars);
+});
+
+//app.get("/", (req, res) => {
+//    res.end("Hello!");
+//});
+//
+//app.get("/urls.json", (req, res) => {
+//    res.json(urlDatabase);
+//});
+//
+//app.get("/hello", (req, res) => {
+//    res.end("<html><body>Hello <b>World</b></body></html>\n");
+//});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+});
